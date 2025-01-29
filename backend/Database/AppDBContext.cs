621b53d4d1d5c50d3a215d52d9fb4db34d71cbff
Rohit Dham <rdhammpls@gmail.com>
2025-01-28 22:05:48 -0600

    Added React.js frontend and ASP.NET Core backend in C#

---
+++ backend/Database/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Employee> Employees { get; set; }
+}
