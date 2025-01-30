58b85d165e0ac1b701af4ac11ddde2d0d2bbc003
Rohit Dham <rdhammpls@gmail.com>
2025-01-29 22:44:14 -0600

    Reconfigured DB

--- backend/Migrations/20250130044221_New DB.Designer.cs
+++ backend/Migrations/20250130044221_New DB.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.Data;
 
@@ -10,9 +11,11 @@ using backend.Data;
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250130044221_New DB")]
+    partial class NewDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Models/Entities/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace backend.Models.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public String description { get; set; }
+        public String category {  get; set; }
+        public double amount { get; set; }
+        public String date { get; set; }
+    }
+}
